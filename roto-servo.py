a46dc3e29d0d963ef49ca0d5748101184548589b
E. A. Graham Jr <10370165+EAGrahamJr@users.noreply.github.com>
2023-08-13 11:18:48 -0700

    Moving stuff around; dorking with servos

--- roto-servo.py
+++ roto-servo.py
@@ -7,20 +7,25 @@
 """CircuitPython Essentials Servo standard servo example"""
 import time
 import board
-import pwmio
+#import pwmio
 from adafruit_motor import servo
+from adafruit_pca9685 import PCA9685
 
 # create a PWMOut object on Pin A2.
-pwm = pwmio.PWMOut(board.D18, duty_cycle=2 ** 15, frequency=50)
+#pwm = pwmio.PWMOut(board.D18, duty_cycle=2 ** 15, frequency=50)
+i2c = board.I2C()
+pca = PCA9685(i2c)
+pca.frequency = 50
+pwm = pca.channels[0]
 
 # Create a servo object, my_servo.
 servo = servo.Servo(pwm)
 servo.set_pulse_width_range(500,2400)
 
 while True:
-    for angle in range(0, 180, 2):  # 0 - 180 degrees, 5 degrees at a time.
+    for angle in range(0, 180, 1):  # 0 - 180 degrees, 5 degrees at a time.
         servo.angle = angle
         time.sleep(0.05)
-    for angle in range(180, 0, -2): # 180 - 0 degrees, 5 degrees at a time.
+    for angle in range(180, 0, -1): # 180 - 0 degrees, 5 degrees at a time.
         servo.angle = angle
         time.sleep(0.05)
