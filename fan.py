ba59f4fa81d1fda6f90d9803feee04209086a366
E. A. Graham Jr <EAGrahamJr@users.noreply.github.com>
2023-08-22 10:41:04 -0700

    Testing out servos on multiple controllers

---
+++ fan.py
@@ -0,0 +1,3 @@
+#!/bin/env python3
+from adafruit_crickit import crickit
+crickit.drive_1.fraction = 1
--- roto-servo.py
+++ roto-servo.py
@@ -1,37 +1,66 @@
 #!/bin/env python3
 
-import time
+from time import sleep
 import board
-#import pwmio
 from adafruit_motor import servo
-from adafruit_pca9685 import PCA9685
 
-# create a PWMOut object on Pin A2.
-#pwm = pwmio.PWMOut(board.D18, duty_cycle=2 ** 15, frequency=50)
 i2c = board.I2C()
-pca = PCA9685(i2c)
-pca.frequency = 50
-pwm0 = pca.channels[0]
-pwm1 = pca.channels[1]
 
-servo0 = servo.Servo(pwm0, actuation_range=180)
-servo0.set_pulse_width_range(500,2400)
+def move_servo(servo, angle: float, rate: float = 0.015):
+        """Move a servo to a certain angle.
 
-servo1 = servo.Servo(pwm1, actuation_range=180)
-servo1.set_pulse_width_range(500,2400)
+        Args:
+            servo (Servo): the servo
+            angle (float): where to move to
+            rate (float): pause between steps in seconds or fractions thereof
+        """
+        current = int(servo.angle)
+        if angle > current:
+            delta = 1
+        else:
+            delta = -1
 
-try:
-    while True:
-        for angle in range(0, 180, 1):  # 0 - 180 degrees, 5 degrees at a time.
-            servo0.angle = angle
-            if angle % 15 == 0:
-                servo1.angle = angle
-            time.sleep(1)
-        for angle in range(180, 0, -1): # 180 - 0 degrees, 5 degrees at a time.
-            servo0.angle = angle
-            if angle % 15 == 0:
-                servo1.angle = angle
-            time.sleep(1)
-except:
-    servo0.angle = 0
-    servo1.angle = 0
+        for i in range(current, angle, delta):
+            servo.angle = i
+            sleep(rate)
+
+        servo.angle = angle
+
+def gpio_servo():
+    import pwmio
+    pwm = pwmio.PWMOut(board.D18, duty_cycle=2 ** 15, frequency=50)
+    return servo.Servo(pwm, actuation_range=180)
+
+def hat_servo():
+    from adafruit_pca9685 import PCA9685
+    pca = PCA9685(i2c)
+    pca.frequency = 50
+    pwm = pca.channels[1]
+    return servo.Servo(pwm, actuation_range=180)
+
+def crickit_servo():
+    from adafruit_crickit import crickit
+    return crickit.servo_4
+
+def run_angles():
+    for j in range(5):
+        for i in [0,180,90]:
+            print(f"Angle = {i}")
+            move_servo(myServo, i)
+            sleep(2)
+
+# myServo = crickit_servo()
+myServo = hat_servo()
+# myServo = gpio_servo()
+myServo.angle = 90 # just to "put" it somewhere
+
+# print("Using default 750-2250")
+# run_angles()
+
+print("Using SG90 500-2400")
+myServo.set_pulse_width_range(500,2400)
+run_angles()
+
+print("Using extended 500-2500")
+myServo.set_pulse_width_range(500,2500)
+run_angles()
