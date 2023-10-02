b8a265d59cd0a34203ad7c559f67c9db7db8e9fc
E. A. Graham Jr <10370165+EAGrahamJr@users.noreply.github.com>
2023-10-02 13:02:38 -0700

    Mux works

--- multiplexer/multiport.py
+++ multiplexer/multiport.py
@@ -4,12 +4,10 @@
 import board
 import adafruit_tca9548a
 import adafruit_vcnl4040
-import displayio
-import adafruit_displayio_ssd1306
 import adafruit_ssd1306
 from adafruit_display_text import label
 from time import sleep
-import terminalio
+from PIL import Image, ImageDraw, ImageFont
 
 WIDTH = 128
 HEIGHT = 32  # Change to 64 if needed
@@ -20,49 +18,65 @@ i2c = board.I2C()
 # Create the TCA9548A object and give it the I2C bus
 tca = adafruit_tca9548a.TCA9548A(i2c)
 
-# sensor
+# sensor = adafruit_vcnl4040.VCNL4040(i2c)
 sensor = adafruit_vcnl4040.VCNL4040(tca[0])
-# TODO display causes too muck impedeance on the i2c bus
-# display_bus = displayio.I2CDisplay(tca[7], device_address=0x3C)
-# display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=32)
+# display = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c, addr=0x3C)
+# display1 = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, tca[7], addr=0x3C)
+# display2 = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, tca[6], addr=0x3C)
 
-# # Draw a label
-# text = "Hello World!"
-# text_area = label.Label(
-#     terminalio.FONT, text=text, color=0xFFFFFF, x=28, y=HEIGHT // 2 - 1
-# )
+# Create blank image for drawing.
+image1 = Image.new("1", (WIDTH, HEIGHT))
+draw1 = ImageDraw.Draw(image1)
+image2 = Image.new("1", (WIDTH, HEIGHT))
+draw2 = ImageDraw.Draw(image2)
 
-# def show_display():
+# Load a font in 2 different sizes.
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 28)
+font2 = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 14)
 
-#     # Make the display context
-#     splash = displayio.Group()
-#     display.show(splash)
+def clear_image():
+    draw1.rectangle((0,0,WIDTH,HEIGHT), fill=0)
+    display1.show()
+    draw2.rectangle((0,0,WIDTH,HEIGHT), fill=0)
+    display2.show()
 
-#     color_bitmap = displayio.Bitmap(WIDTH, HEIGHT, 1)
-#     color_palette = displayio.Palette(1)
-#     color_palette[0] = 0xFFFFFF  # White
+# clear_image()
 
-#     bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
-#     splash.append(bg_sprite)
+# Draw the text
+# draw1.text((0, 0), "Hello!", font=font2, fill=255)
+# draw2.text((0, 17), "Hello!", font=font2, fill=255)
 
-#     # Draw a smaller inner rectangle
-#     inner_bitmap = displayio.Bitmap(WIDTH - BORDER * 2, HEIGHT - BORDER * 2, 1)
-#     inner_palette = displayio.Palette(1)
-#     inner_palette[0] = 0x000000  # Black
-#     inner_sprite = displayio.TileGrid(
-#         inner_bitmap, pixel_shader=inner_palette, x=BORDER, y=BORDER
-#     )
-#     splash.append(inner_sprite)
+# Display image -- cheating a bit
+def show_image():
+    display1.image(image1)
+    display1.show()
+    display2.image(image2)
+    display2.show()
 
-#     splash.append(text_area)
+#show_image()
+#sleep(2)
+#clear_image()
 
-# show_display()
-# sleep(2)
+def do_it():
+    # print(f"reading =============================================================")
+    reading = f"********* Lux: {sensor.lux}"
+    print(reading)
+#    print("display on 7 ===========================================================")
+#    draw1.text((0, 0),reading, font=font2, fill=255)
+#    display1.image(image1)
+#    display1.show()
+#    print("display on 6 ===========================================================")
+#    draw2.text((0,0), f"Prox: {sensor.proximity}", font=font2, fill=255)
+#    display2.image(image2)
+#    display2.show()
 
 while True:
     try:
-        print(f"Proximity: {sensor.proximity}")
-        # text_area.text = f"Proximity: {sensor.proximity}"
-        sleep(1)
+        # clear_image()
+        do_it()
+        sleep(5)
     except KeyboardInterrupt:
         break
+# do_it()
+# sleep(5)
+# clear_image()
