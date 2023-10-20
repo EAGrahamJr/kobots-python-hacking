d2a2b992422bd6b6d4bfd8c18bac41533ba56fd1
E. A. Graham Jr <EAGrahamJr@users.noreply.github.com>
2023-10-20 09:26:22 -0700

    Switch to VENV for bookworm

--- working/minioled.py
+++ working/minioled.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 
-from board import SCL, SDA
+# from board import SCL, SDA
 from time import sleep
 import busio
+import board
 
 # Import the SSD1306 module.
 import adafruit_ssd1306
 
 
 # Create the I2C interface.
-i2c = busio.I2C(SCL, SDA)
+i2c = busio.I2C(board.SCL, board.SDA)
 
 # Create the SSD1306 OLED class.
 # The first two parameters are the pixel width and pixel height.  Change these
