817df570a6f9313f6d183a7e8b7e2cd9523ed58b
E. A. Graham Jr <10370165+EAGrahamJr@users.noreply.github.com>
2024-04-11 17:18:18 -0700

    rename foo correctly; add marvin servo hat stuff

---
+++ marvin_servo.py
@@ -0,0 +1,14 @@
+from edlib import rotoservo as rs
+
+s1 = rs.mg90s(rs.hat(0))
+s2 = rs.mg90s(rs.hat(1))
+s3 = rs.mg90s(rs.hat(2))
+s4 = rs.mg90s(rs.hat(3))
+
+def home():
+    s4.angle = 0
+    s2.angle = 0
+    s3.angle = 0
+    s1.angle = 0
+
+    
