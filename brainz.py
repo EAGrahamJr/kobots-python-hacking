c5d9e8f66f423b90ca443ff1bda6d36a49f16a51
E. A. Graham Jr <EAGrahamJr@users.noreply.github.com>
2024-05-23 13:30:56 -0700

    Add a default speed for the crummy stepper

--- brainz.py
+++ brainz.py
@@ -12,6 +12,7 @@ s4 = rs.mg90s(rs.crickit(4))
 
 # steppers
 step1 = rm.RotoStepper(crickit.drive_stepper_motor) # ONCE = 2048
+step1.speed = .01
 step1.release()
 # note: currently takes about 16000 steps to move the scrissor up and down
 step2 = rm.RotoStepper(crickit.stepper_motor)     # ONCE = 400
