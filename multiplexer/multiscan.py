830420eb78ac86e90207f42c1af417dfc55d0844
E. A. Graham Jr <EAGrahamJr@users.noreply.github.com>
2024-06-11 08:45:16 -0700

    Ignore multiplexer address on scan

--- multiplexer/multiscan.py
+++ multiplexer/multiscan.py
@@ -15,5 +15,5 @@ for channel in range(8):
     if tca[channel].try_lock():
         print("Channel {}:".format(channel), end="")
         addresses = tca[channel].scan()
-        print([hex(address) for address in addresses])
+        print([hex(address) for address in addresses if address != 0x70])
         tca[channel].unlock()
