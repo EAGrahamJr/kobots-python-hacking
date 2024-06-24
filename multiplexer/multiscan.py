36eb8c2e78c405bf819d1551ecd603e5bcd2e864
E. A. Graham Jr <EAGrahamJr@users.noreply.github.com>
2024-06-24 10:54:49 -0700

    Put I2C scan in a loop to test hot-plug

--- multiplexer/multiscan.py
+++ multiplexer/multiscan.py
@@ -3,6 +3,7 @@
 # This example shows using TCA9548A to perform a simple scan for connected devices
 import board
 import adafruit_tca9548a
+from time import sleep
 
 # Create I2C bus as normal
 i2c = board.I2C()  # uses board.SCL and board.SDA
@@ -11,9 +12,19 @@ i2c = board.I2C()  # uses board.SCL and board.SDA
 # Create the TCA9548A object and give it the I2C bus
 tca = adafruit_tca9548a.TCA9548A(i2c)
 
-for channel in range(8):
-    if tca[channel].try_lock():
-        print("Channel {}:".format(channel), end="")
-        addresses = tca[channel].scan()
-        print([hex(address) for address in addresses if address != 0x70])
-        tca[channel].unlock()
+# !!!!!NOTE!!!!! this particular multiplexer has a "bad" channel 2 and will "lock" the I2C bus
+while True:
+    try:
+        print("2 second sleep")
+        sleep(2)
+        for channel in range(8):
+            if channel != 2:
+                if tca[channel].try_lock():
+                    print("Channel {}:".format(channel), end="")
+                    addresses = tca[channel].scan()
+                    print([hex(address) for address in addresses if address != 0x70])
+                    tca[channel].unlock()
+    except KeyboardInterrupt:
+        break
+    except Exception as e:
+        print("Error: ",e)
