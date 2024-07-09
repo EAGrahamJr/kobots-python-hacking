fb4811e90232c12e164d53be2dfb113769f98487
E. A. Graham Jr <10370165+EAGrahamJr@users.noreply.github.com>
2024-07-09 12:04:36 -0700

    Simple PWM light

--- marvin.py
+++ marvin.py
@@ -1,6 +1,7 @@
 from edlib import rotoservo as rs
 from edlib import rotomotor as rm
 import board
+from digitalio import DigitalInOut,Direction
 
 s1 = rs.mg90s(rs.hat(0))
 s2 = rs.mg90s(rs.hat(1))
@@ -11,8 +12,12 @@ s5 = rs.mg90s(rs.hat(4))
 # step1 = rm.digitalStepper(board.D17,board.D18,board.D10,board.D9)
 step1 = rm.digitalStepper(board.D27,board.D21,board.D13,board.D26)
 
+nood = DigitalInOut(board.D4)
+nood.direction = Direction.OUTPUT
+
 def home():
     step1.release()
+    nood.value = False
     s4.angle = 0
     s2.angle = 0
     s5.angle = 0
