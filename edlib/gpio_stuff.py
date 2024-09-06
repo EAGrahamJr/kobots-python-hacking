8c47cdf04374b1dd85700f669c2e93ec0e30eeec
E. A. Graham Jr <EAGrahamJr@users.noreply.github.com>
2024-09-06 10:32:56 -0700

    Comments and a servo

--- edlib/gpio_stuff.py
+++ edlib/gpio_stuff.py
@@ -1,8 +1,16 @@
+import board
 import pwmio
 import digitalio
+from adafruit_motor.servo import Servo
 
 class LED:
+    """Dimmable LED """
     def __init__(self, pin) -> None:
+        """Create the GPIO LED using PWM
+
+        Args:
+            pin (Pin): which GPIO pin
+        """
         self._out = pwmio.PWMOut(pin)
         self._out.enabled = True
         self._brightness = 0
@@ -19,7 +27,13 @@ class LED:
         self._out.duty_cycle = int((b / 100.0) * 65535)
 
 class Button:
+    """Manages a digital pin as a "button" """
     def __init__(self, pin) -> None:
+        """Create the button
+
+        Args:
+            pin (Pin): which GPIO pin
+        """
         self._in = digitalio.DigitalInOut(pin)
         self._in.direction = digitalio.Direction.INPUT
         self._in.pull = digitalio.Pull.UP
@@ -35,3 +49,17 @@ class Button:
             self._pressed = True
         elif not self.value:
             self._pressed = False
+
+def gpio_servo(pin=board.D18) -> Servo:
+    """Create a servo for a GPIO pin
+
+    Args:
+        pin (Pin, optional): which pin to use. Defaults to board.D18 since it's
+        the most common hardware PWM pin
+
+    Returns:
+        Servo: an Adafruit "Servo" for the pin
+    """
+    import pwmio
+    pwm = pwmio.PWMOut(pin, duty_cycle=2 ** 15, frequency=50)
+    return Servo(pwm, actuation_range=180)
