e8b7831ccaa1388c59bbc52dfb0e7c5848497ae9
E. A. Graham Jr <10370165+EAGrahamJr@users.noreply.github.com>
2024-09-06 10:04:33 -0700

    Added a switch that isn't currently used

--- edlib/gpio_stuff.py
+++ edlib/gpio_stuff.py
@@ -1,4 +1,5 @@
 import pwmio
+import digitalio
 
 class LED:
     def __init__(self, pin) -> None:
@@ -16,3 +17,21 @@ class LED:
             raise ValueError("b must be between 0 and 100")
         self._brightness = b
         self._out.duty_cycle = int((b / 100.0) * 65535)
+
+class Button:
+    def __init__(self, pin) -> None:
+        self._in = digitalio.DigitalInOut(pin)
+        self._in.direction = digitalio.Direction.INPUT
+        self._in.pull = digitalio.Pull.UP
+        self._pressed = False
+
+    @property
+    def value(self) -> bool:
+        return not self._in.value
+
+    @property
+    def pressed(self) -> bool:
+        if self.value and not self._pressed:
+            self._pressed = True
+        elif not self.value:
+            self._pressed = False
--- marvin.py
+++ marvin.py
@@ -1,29 +1,38 @@
 from edlib import rotoservo as rs
 from edlib import rotomotor as rm
-from edlib import gpio_stuff as jeep
 import board
 from digitalio import DigitalInOut,Direction
 import pwmio
+from adafruit_motor import stepper
+import adafruit_vl6180x
 
-s1 = rs.mg90s(rs.hat(0))
-s2 = rs.mg90s(rs.hat(1))
-s3 = rs.mg90s(rs.hat(2))
-s4 = rs.mg90s(rs.hat(3))
+s1 = rs.mg90s(rs.hat(0)) # a1
+s2 = rs.mg90s(rs.hat(1)) # a2
+s3 = rs.mg90s(rs.hat(2)) # twist
+s4 = rs.mg90s(rs.hat(3)) # grab
 s5 = rs.mg90s(rs.hat(4))
 
 # step1 = rm.digitalStepper(board.D17,board.D18,board.D10,board.D9)
-step1 = rm.digitalStepper(board.D27,board.D21,board.D13,board.D26)
+step1 = rm.digitalStepper(board.D27,board.D21,board.D13,board.D26) # ONCE = 4096
+step1.speed = .005
+step1.release()
 
 # nood = pwmio.PWMOut(board.D4)
 # nood = DigitalInOut(board.D4)
 # nood.direction = Direction.OUTPUT
 nood = jeep.LED(board.D4)
+#switch = jeep.Button(board.D23)
+sensor = adafruit_vl6180x.VL6180X(board.I2C())
 
 def home():
+    nood.brightness = 75
     step1.release()
-    nood.brightness = 0
     s4.angle = 0
-    s2.angle = 0
-    s5.angle = 0
     s3.angle = 0
+
+    if s2.angle != 0:
+        s2.angle = 30
     s1.angle = 0
+    s2.angle = 0
+
+    nood.brightness = 0
