7b5a0107d7802b102d839085d4614ac7c2336d04
E. A. Graham Jr. <10370165+EAGrahamJr@users.noreply.github.com>
2025-09-07 09:21:19 -0700

    Comments and bouncy

--- pybot/easefunc.py
+++ pybot/easefunc.py
@@ -4,9 +4,21 @@ from typing import Callable
 
 # Easing curve examples
 def linear(t): return t
-def ease_in_quad(t): return t * t
-def ease_out_quad(t): return t * (2 - t)
-def ease_in_out_sine(t): return -(math.cos(math.pi * t) - 1) / 2
+def ease_in_quad(t): return t * t # Gentle launch
+def ease_out_quad(t): return t * (2 - t) # Soft landing
+def ease_in_out_sine(t): return -(math.cos(math.pi * t) - 1) / 2 # smooth
+def ease_out_bounce(t: float) -> float: # bouncing ball
+    if t < 1 / 2.75:
+        return 7.5625 * t * t
+    elif t < 2 / 2.75:
+        t -= 1.5 / 2.75
+        return 7.5625 * t * t + 0.75
+    elif t < 2.5 / 2.75:
+        t -= 2.25 / 2.75
+        return 7.5625 * t * t + 0.9375
+    else:
+        t -= 2.625 / 2.75
+        return 7.5625 * t * t + 0.984375
 
 async def ease_value(
         start: float,
